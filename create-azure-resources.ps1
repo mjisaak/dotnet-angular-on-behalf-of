08b1e0838e4d9f31ca6039bb75f679c3355ec9a5
Stefan Kürzeder <info@stivik.me>
2021-10-11 16:51:16 +0200

    Dump access token

--- create-azure-resources.ps1
+++ create-azure-resources.ps1
@@ -7,6 +7,7 @@ $backendApp = 'on-behalf-of-backend-web'
 az group create --name $rg --location $location
 az appservice plan create --name $appServicePlan --resource-group $rg --sku S1 --is-linux
 az webapp create --resource-group $rg --plan $appServiceplan --name $frontendApp --runtime '"NODE|14-lts"' --startup-file "pm2 serve /home/site/wwwroot --no-daemon --spa"
+az webapp config appsettings set --resource-group $rg --name $frontendApp --settings SCM_DO_BUILD_DURING_DEPLOYMENT=FALSE
 az webapp create --resource-group $rg --plan $appServiceplan --name $backendApp --runtime '"DOTNET|6.0"' --deployment-local-git --query deploymentLocalGitUrl
 
 # pm2 serve /home/site/wwwroot --no-daemon --spa 
--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  public token: Observable<{ access_token: string, id_token: string }[]> | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.token = this.http.get<{ access_token: string, id_token: string }[]>('/.auth/me')
+      .pipe(
+        tap(tokens => {
+          console.log(tokens[0])
+        })
+      );
+  }
 }
