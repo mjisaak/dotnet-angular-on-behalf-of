76e928e31a3ec8dce93118ce854957912af547a0
Stefan Kürzeder <info@stivik.me>
2021-10-11 17:25:12 +0200

    Addendum to last commit

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +9,39 @@ import { tap } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public token: Observable<{ access_token: string, id_token: string }[]> | null = null;
+  private accessToken: string = "";
+  public tokenResponse: Observable<string> | null = null;
+  public apiResponse: Observable<string> | null = null;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.token = this.http.get<{ access_token: string, id_token: string }[]>('/.auth/me')
+    this.tokenResponse = this.http.get<{ access_token: string, id_token: string }[]>('/.auth/me')
       .pipe(
         tap(tokens => {
           console.log(tokens[0])
+          this.accessToken = tokens[0].access_token;
+        }),
+        map(tokens => {
+          return JSON.stringify(tokens[0]);
+        }),
+        catchError((err: HttpErrorResponse) => {
+          return of(`Error: ${err.status} = ${err.message}`);
+        })
+      );
+  }
+
+  public callApi(): void {
+    this.apiResponse = this.http.get<{}>('https://on-behalf-of-backend-web.azurewebsites.net/WeatherForecast', { headers: { 'Authorization': `Bearer ${this.accessToken}` } })
+      .pipe(
+        tap(result => {
+          console.log(result)
+        }),
+        map(result => {
+          return JSON.stringify(result);
+        }),
+        catchError((err: HttpErrorResponse) => {
+          return of(`Error: ${err.status} = ${err.message}`);
         })
       );
   }
