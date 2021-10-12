4e41377c50862dcce3b954bc4a887e7a0d802e9e
Stefan Kürzeder <info@stivik.me>
2021-10-12 14:17:10 +0200

    Improved code.

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { defer, Observable, of, ReplaySubject } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 const apiUrl = 'https://on-behalf-of-backend-web.azurewebsites.net';
+
+export enum Status {
+  Loading,
+  Success,
+}
 
 @Component({
   selector: 'app-root',
@@ -11,60 +16,95 @@ const apiUrl = 'https://on-behalf-of-backend-web.azurewebsites.net';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  public Status = Status;
+
   private idToken: string = "";
   private accessToken: string = "";
-  public tokenResponse: Observable<string> | null = null;
-  public backendResponse: Observable<string> | null = null;
-  public graphResponse: Observable<string> | null = null;
-  public delegatedResponse: Observable<string> | null = null;
 
-  public apiUrl: string = "https://graph.microsoft.com/v1.0/me";
+  public tokenResponse: { data: Observable<string>; status: ReplaySubject<Status>; } | null = null;
+  public backendResponse: { data: Observable<string>; status: ReplaySubject<Status>; } | null = null;
+  public graphResponse: { data: Observable<string>; status: ReplaySubject<Status>; } | null = null;
+  public delegatedResponse: { data: Observable<string>; status: ReplaySubject<Status>; } | null = null;
+
+  public targetUrl: string = "https://graph.microsoft.com/v1.0/me";
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.tokenResponse = this.http.get<{ access_token: string, id_token: string }[]>('/.auth/me')
+    const status = new ReplaySubject<Status>();
+    var request = this.http.get<{ access_token: string, id_token: string }[]>('/.auth/me')
       .pipe(
         tap(tokens => {
           this.idToken = tokens[0].id_token;
           this.accessToken = tokens[0].access_token;
+          status.next(Status.Success);
         }),
         map(tokens => {
           return JSON.stringify(tokens[0]);
         }),
         catchError((err: HttpErrorResponse) => {
           return of(`Error: ${err.status} = ${err.message}`);
-        })
+        }),
       );
+    
+    this.tokenResponse = {
+        data: defer(() => {
+          status.next(Status.Loading);
+          return request;
+        }), status
+      };
   }
 
   public callBackendApi(): void {
-    this.backendResponse = this.http.get<{}>(`${apiUrl}/WeatherForecast`, { headers: { 'Authorization': `Bearer ${this.idToken}` } })
+    const status = new ReplaySubject<Status>();
+    var request = this.http.get<{}>(`${apiUrl}/WeatherForecast`, { headers: { 'Authorization': `Bearer ${this.idToken}` } })
       .pipe(
         map(result => {
           return JSON.stringify(result);
         }),
         catchError((err: HttpErrorResponse) => {
           return of(`Error: ${err.status} = ${err.message}`);
+        }),
+        tap(_ => {
+          status.next(Status.Success);
         })
       );
+
+    this.backendResponse = {
+      data: defer(() => {
+        status.next(Status.Loading);
+        return request;
+      }), status
+    };
   }
 
   public callGraphApi(): void {
-    this.graphResponse = this.http.get<{}>('https://graph.microsoft.com/v1.0/me', { headers: { 'Authorization': `Bearer ${this.accessToken}` } })
+    const status = new ReplaySubject<Status>();
+    var request = this.http.get<{}>('https://graph.microsoft.com/v1.0/me', { headers: { 'Authorization': `Bearer ${this.accessToken}` } })
       .pipe(
         map(result => {
           return JSON.stringify(result);
         }),
         catchError((err: HttpErrorResponse) => {
           return of(`Error: ${err.status} = ${err.message}`);
+        }),
+        tap(_ => {
+          status.next(Status.Success);
         })
       );
+
+    this.graphResponse = {
+      data: defer(() => {
+        status.next(Status.Loading);
+        return request;
+      }), status
+    };
   }
 
   public callGraphThroughBackendApi(): void {
-    this.delegatedResponse = this.http.post<{}>(`${apiUrl}/Delegated`, 
-      { api_url: apiUrl, access_token: this.accessToken }, 
+    const status = new ReplaySubject<Status>();
+    var request = this.http.post<{}>(`${apiUrl}/Delegated`,
+      { api_url: this.targetUrl, access_token: this.accessToken },
       { headers: { 'Authorization': `Bearer ${this.idToken}`, 'Content-Type': 'application/json' } }
     )
       .pipe(
@@ -73,8 +113,18 @@ export class AppComponent implements OnInit {
         }),
         catchError((err: HttpErrorResponse) => {
           return of(`Error: ${err.status} = ${err.message}`);
+        }),
+        tap(_ => {
+          status.next(Status.Success);
         })
       );
+
+    this.delegatedResponse = {
+      data: defer(() => {
+        status.next(Status.Loading);
+        return request;
+      }), status
+    };
   }
 }
 
