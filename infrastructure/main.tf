89590d29b92ddc50bc8b5e1ac8dd46fc89701aff
Stefan Kürzeder <info@stivik.me>
2021-10-15 11:15:14 +0200

    Added empty state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,9 +25,9 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.default_location
 }
-
-data "azurerm_client_config" "current" {}
